b44dedc0ddf6efb4ac2db5733542a851b5db3e60
Jason Lampel <jason@abettermap.com>
2017-07-29 13:59:11 -0600

    Beautify Meetup JS

--- assets/js/group-calendar.js
+++ assets/js/group-calendar.js
@@ -1,95 +1,100 @@
 $ = jQuery;
 
 var $parameters_grp = {
-  group_urlname: "Code-for-Fort-Collins",
-  after: "-6m",
-  before: "6m",
-  _width: 400,
-  _height: 290,
-  _name: "Group Calendar",
-  _description: "Lists recent and upcoming meetups for the specified group. The style is very basic so it can be easily adapted to match your site."
+    group_urlname: "Code-for-Fort-Collins",
+    after: "-6m",
+    before: "6m",
+    _width: 400,
+    _height: 290,
+    _name: "Group Calendar",
+    _description: "Lists recent and upcoming meetups for the specified group. The style is very basic so it can be easily adapted to match your site."
 };
 var $queries = {
-  events: function() {
-    return api_call("/events", {
-      after: $parameters_grp.after,
-      before: $parameters_grp.before,
-      group_urlname: $parameters_grp.group_urlname,
-      page: 6,
-      desc: true
-    });
-  },
-  groups: function() {
-    return api_call("/groups", {
-      group_urlname: $parameters_grp.group_urlname
-    });
-  }
+    events: function() {
+        return api_call("/events", {
+            after: $parameters_grp.after,
+            before: $parameters_grp.before,
+            group_urlname: $parameters_grp.group_urlname,
+            page: 6,
+            desc: true
+        });
+    },
+    groups: function() {
+        return api_call("/groups", {
+            group_urlname: $parameters_grp.group_urlname
+        });
+    }
 };
 
 $(function() {
     target = $("#meetups");
     $.getJSON($queries.groups(), function(data) {
-        if (data.results) for (var i in data.results) {
-          $("#group_name").text(data.results[i].name)
+        if (data.results) {
+            for (var i in data.results) {
+                $("#group_name").text(data.results[i].name)
+            }
         }
     });
     $.getJSON($queries.events(), function(data) {
-      target.empty();
-      if (data.status && data.status.match(/^200/) == null) {
-        $("#meetup_listing").empty();
-      } else for (var i in data.results.reverse()) {
-        ev = data.results[data.results.length - i - 1];
-        ds = ev.time.match(/\w+ (\w+) (\d+) (\d+):(\d+):\d+ \w+ (\d+)/)
-        year = ds.pop();
-        minute = ds.pop();
-        hour = ds.pop();
-        day = ds.pop();
-        month = ds.pop();
-        date = month + " " + day + ", " + year + " @ " + hour % 12 + ":" +
-          minute + (hour < 12 ? "am" : "pm");
-        li = $('<li></li>');
-        li.append($('<div style="font-weight: bold"></div>').append(ev.name))
-        go = $('<div></div>').append($('<a target="_TOP" href="' + ev.event_url + '"></a>').append(date))
-        if (ev.status == 'upcoming')
-          go.append(' | ')
-            .append($('<a target="_TOP" href="' + ev.event_url + '" style="background: red; color: white; text-decoration: none; font-size: smaller; font-weight: bold; padding: .3em .5em; border: white; -webkit-border-radius: .3em; -moz-border-radius: .3em"></a>')
-              .append("RSVP")
-            );
-        target.append(li.append(go));
-      }
+        target.empty();
+        if (data.status && data.status.match(/^200/) == null) {
+            $("#meetup_listing").empty();
+        } else
+            for (var i in data.results.reverse()) {
+                ev = data.results[data.results.length - i - 1];
+                ds = ev.time.match(/\w+ (\w+) (\d+) (\d+):(\d+):\d+ \w+ (\d+)/)
+                year = ds.pop();
+                minute = ds.pop();
+                hour = ds.pop();
+                day = ds.pop();
+                month = ds.pop();
+                date = month + " " + day + ", " + year + " @ " + hour % 12 + ":" +
+                    minute + (hour < 12 ? "am" : "pm");
+                li = $('<li></li>');
+                li.append($('<div style="font-weight: bold"></div>').append(ev.name))
+                go = $('<div></div>').append($('<a target="_TOP" href="' + ev.event_url + '"></a>').append(date))
+                if (ev.status == 'upcoming')
+                    go.append(' | ')
+                    .append($('<a target="_TOP" href="' + ev.event_url + '" style="background: red; color: white; text-decoration: none; font-size: smaller; font-weight: bold; padding: .3em .5em; border: white; -webkit-border-radius: .3em; -moz-border-radius: .3em"></a>')
+                        .append("RSVP")
+                    );
+                target.append(li.append(go));
+            }
     });
-  });
+});
 
 $(function() {
     target = $("#meetups");
     $.getJSON($queries.groups(), function(data) {
-        if (data.results) for (var i in data.results) {
-          $("#group_name").text(data.results[i].name)
-        }
+        if (data.results)
+            for (var i in data.results) {
+                $("#group_name").text(data.results[i].name)
+            }
     });
     $.getJSON($queries.events(), function(data) {
-      target.empty();
-      if (data.status && data.status.match(/^200/) == null) {
-        $("#meetup_listing").empty();
-      } else for (var i in data.results.reverse()) {
-        ev = data.results[data.results.length - i - 1];
-        ds = ev.time.match(/\w+ (\w+) (\d+) (\d+):(\d+):\d+ \w+ (\d+)/)
-        year = ds.pop();
-        minute = ds.pop();
-        hour = ds.pop();
-        day = ds.pop();
-        month = ds.pop();
-        date = month + " " + day + ", " + year + " @ " + hour % 12 + ":" +
-          minute + (hour < 12 ? "am" : "pm");
-        li = $('<li></li>');
-        li.append($('<div style="font-weight: bold"></div>').append(ev.name))
-        go = $('<div></div>').append($('<a target="_TOP" href="' + ev.event_url + '"></a>').append(date))
-        if (ev.status == 'upcoming')
-          go.append(' | ')
-            .append($('<a target="_TOP" href="' + ev.event_url + '" style="background: red; color: white; text-decoration: none; font-size: smaller; font-weight: bold; padding: .3em .5em; border: white; -webkit-border-radius: .3em; -moz-border-radius: .3em"></a>')
-              .append("RSVP")
-            );
-        target.append(li.append(go));
-      }
+        target.empty();
+        if (data.status && data.status.match(/^200/) == null) {
+            $("#meetup_listing").empty();
+        } else
+            for (var i in data.results.reverse()) {
+                ev = data.results[data.results.length - i - 1];
+                ds = ev.time.match(/\w+ (\w+) (\d+) (\d+):(\d+):\d+ \w+ (\d+)/)
+                year = ds.pop();
+                minute = ds.pop();
+                hour = ds.pop();
+                day = ds.pop();
+                month = ds.pop();
+                date = month + " " + day + ", " + year + " @ " + hour % 12 + ":" +
+                    minute + (hour < 12 ? "am" : "pm");
+                li = $('<li></li>');
+                li.append($('<div style="font-weight: bold"></div>').append(ev.name))
+                go = $('<div></div>').append($('<a target="_TOP" href="' + ev.event_url + '"></a>').append(date))
+                if (ev.status == 'upcoming')
+                    go.append(' | ')
+                    .append($('<a target="_TOP" href="' + ev.event_url + '" style="background: red; color: white; text-decoration: none; font-size: smaller; font-weight: bold; padding: .3em .5em; border: white; -webkit-border-radius: .3em; -moz-border-radius: .3em"></a>')
+                        .append("RSVP")
+                    );
+                target.append(li.append(go));
+            }
     });
-  });
+});
--- assets/js/upcoming-meetups.js
+++ assets/js/upcoming-meetups.js
@@ -1,153 +1,157 @@
 var $parameters = {
-  urlname: "Code-for-Fort-Collins",
-  width: 225,
-  height:570,
-  _name: "Meetup Group Stats",
-  _description: "Shows basic stats on your favorite Meetup group."
+    urlname: "Code-for-Fort-Collins",
+    width: 425,
+    height: 570,
+    _name: "Meetup Group Stats",
+    _description: "Shows basic stats on your favorite Meetup group."
 };
 var $queries = {
-  groups: function() {
-    return mup_widget.api_call("/2/groups", {group_urlname: $parameters.urlname});
-  },
-  events: function() {
-    return mup_widget.api_call("/2/events", {group_urlname: $parameters.urlname, page: '1'});
-  }
+    groups: function() {
+        return mup_widget.api_call("/2/groups", {
+            group_urlname: $parameters.urlname
+        });
+    },
+    events: function() {
+        return mup_widget.api_call("/2/events", {
+            group_urlname: $parameters.urlname,
+            page: '5'
+        });
+    }
 };
 
 mup_widget.with_jquery(function($, ctx) {
-    var	group = '',
-            months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
-            addLink = function(content, link) {
-                    return '<a target="_top" href="' + link + '">' + content + '</a>';
-      },
-    addImage = function(src, alt) {
-       return src == "" ? '' : '<div class="mup-img-wrap"><img src="'+src+'" width="'+($parameters.width - 50)+'" alt="'+alt+'" class="mup-img"/></div>';
-    },
-            addStarRating = function(rating) {
-                    var base_url = 'https://a248.e.akamai.net/secure.meetupstatic.com/img/03784007994490629714917/star_';
-                    var starlink = '';
-                    if (rating == 0) {
-                        return 'Not Yet Rated';
-                    }else if (rating < 1.25) {
-                        starlink = "100.png";
-                    }else if (rating < 1.75) {
-                        starlink = "150.png";
-                    }else if (rating < 2.25) {
-                        starlink = "200.png";
-                    }else if (rating < 2.75) {
-                        starlink = "250.png";
-                    }else if (rating < 3.25) {
-                        starlink = "300.png";
-                    }else if (rating < 3.75) {
-                        starlink = "350.png";
-                    }else if (rating < 4.25) {
-                        starlink = "400.png";
-                    }else if (rating < 4.75) {
-                        starlink = "450.png";
-                    }else {
-                        starlink = "500.png";
-                    }
-                    return '<img src="'+base_url+starlink+'" alt="'+rating+'" />';
+    var group = '',
+        months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+        addLink = function(content, link) {
+            return '<a target="_top" href="' + link + '">' + content + '</a>';
+        },
+        addImage = function(src, alt) {
+            return src == "" ? '' : '<div class="mup-img-wrap"><img src="' + src + '" width="' + ($parameters.width - 50) + '" alt="' + alt + '" class="mup-img"/></div>';
+        },
+        addStarRating = function(rating) {
+            var base_url = 'https://a248.e.akamai.net/secure.meetupstatic.com/img/03784007994490629714917/star_';
+            var starlink = '';
+            if (rating == 0) {
+                return 'Not Yet Rated';
+            } else if (rating < 1.25) {
+                starlink = "100.png";
+            } else if (rating < 1.75) {
+                starlink = "150.png";
+            } else if (rating < 2.25) {
+                starlink = "200.png";
+            } else if (rating < 2.75) {
+                starlink = "250.png";
+            } else if (rating < 3.25) {
+                starlink = "300.png";
+            } else if (rating < 3.75) {
+                starlink = "350.png";
+            } else if (rating < 4.25) {
+                starlink = "400.png";
+            } else if (rating < 4.75) {
+                starlink = "450.png";
+            } else {
+                starlink = "500.png";
+            }
+            return '<img src="' + base_url + starlink + '" alt="' + rating + '" />';
 
-                },
-            addLeadingZero = function( num ) {
-                    return (num < 10) ? ('0' + num) : num;
-                },
-            getFormattedDate = function( millis ) {
-                    var date = new Date( millis );
-                    return  months[date.getMonth()] + ' ' + addLeadingZero( date.getDate() ) + ', ' + date.getFullYear().toString();
-                },
-            getFormattedTime = function( millis ) {
-                    var	time = new Date( millis ),
-                            hours = time.getHours(),
-                            min = time.getMinutes(),
-                            ampm = (hours > 11) ? 'PM' : 'AM';
-                    min = (min < 10) ? ('0' + min) : min;
-                    hours = (hours == 0) ? 1 : hours;
-                    hours = (hours > 12) ? hours-12 : hours;
-                    return hours + ':' + min + ' ' + ampm;
-                },
-            numberFormat = function(nStr){
-                  nStr += '';
-                  x = nStr.split('.');
-                  x1 = x[0];
-                  x2 = x.length > 1 ? '.' + x[1] : '';
-                  var rgx = /(\d+)(\d{3})/;
-                  while (rgx.test(x1))
-                    x1 = x1.replace(rgx, '$1' + ',' + '$2');
-                  return x1 + x2;
-                };
+        },
+        addLeadingZero = function(num) {
+            return (num < 10) ? ('0' + num) : num;
+        },
+        getFormattedDate = function(millis) {
+            var date = new Date(millis);
+            return months[date.getMonth()] + ' ' + addLeadingZero(date.getDate()) + ', ' + date.getFullYear().toString();
+        },
+        getFormattedTime = function(millis) {
+            var time = new Date(millis),
+                hours = time.getHours(),
+                min = time.getMinutes(),
+                ampm = (hours > 11) ? 'PM' : 'AM';
+            min = (min < 10) ? ('0' + min) : min;
+            hours = (hours == 0) ? 1 : hours;
+            hours = (hours > 12) ? hours - 12 : hours;
+            return hours + ':' + min + ' ' + ampm;
+        },
+        numberFormat = function(nStr) {
+            nStr += '';
+            x = nStr.split('.');
+            x1 = x[0];
+            x2 = x.length > 1 ? '.' + x[1] : '';
+            var rgx = /(\d+)(\d{3})/;
+            while (rgx.test(x1))
+                x1 = x1.replace(rgx, '$1' + ',' + '$2');
+            return x1 + x2;
+        };
     $.getJSON($queries.groups(), function(data) {
-    if (data.results.length == 0) {
-        $('.mug-badge', ctx).width($parameters.width);
+        if (data.results.length == 0) {
+            $('.mug-badge', ctx).width($parameters.width);
             $('.mug-badge', ctx).append(
                 '<div class="mup-widget error">\
                         <div class="errorMsg">Oops. No results for "' + $parameters.urlname + '"</div>\
                 </div>');
-    }
-        else {
-        group = data.results[0];
-        $('.mug-badge', ctx).width($parameters.width);
-        $('.mug-badge', ctx).append(
-            '<div class="mup-widget">\
+        } else {
+            group = data.results[0];
+            $('.mug-badge', ctx).width($parameters.width);
+            $('.mug-badge', ctx).append(
+                '<div class="mup-widget">\
                 <div class="mup-bd">\
                     <h3>' + addLink(group.name, group.link) + '</h3>\
-        <h4> <div style="padding-top:5px;"><span class="mup-tlabel">EST. '+ getFormattedDate(group.created)+'</span></div></h4>\
-                    <span class="mup-stats">' + addImage(group["group_photo"] ? group.group_photo.photo_link : "", group.name) + numberFormat(group.members) + '<span class="mup-tlabel"> '+ group.who+'</span></span>\
+        <h4> <div style="padding-top:5px;"><span class="mup-tlabel">EST. ' + getFormattedDate(group.created) + '</span></div></h4>\
+                    <span class="mup-stats">' + addImage(group["group_photo"] ? group.group_photo.photo_link : "", group.name) + numberFormat(group.members) + '<span class="mup-tlabel"> ' + group.who + '</span></span>\
         <span class="mup-stats"><div class="next-event"></div></span>\
-        <h4><span class="mup-button">'+ addLink('JOIN',group.link)+'</span></h4>\
+        <h4><span class="mup-button">' + addLink('JOIN', group.link) + '</span></h4>\
                 </div>\
                 <div class="mup-ft">\
-                    <div class="mup-logo"><div style="float:left;">'+addLink('<img src="https://a248.e.akamai.net/secure.meetupstatic.com/img/84869143793177372874/birddog/everywhere_widget.png">','http://www.meetup.com')+'</div><div style="float:right;"><div style="float:right;">'+addStarRating(group.rating)+'</div><br><div style="float:right;"><span class="mup-tlabel">Group Rating</span></div></div></div>\
-                    <div class="mup-getwdgt">' + addLink('ADD THIS TO YOUR SITE', 'http://www.meetup.com/meetup_api/foundry/#'+$parameters._name.toLowerCase().replace(/ /g,"-")) + '</div>\
+                    <div class="mup-logo"><div style="float:left;">' + addLink('<img src="https://a248.e.akamai.net/secure.meetupstatic.com/img/84869143793177372874/birddog/everywhere_widget.png">', 'http://www.meetup.com') + '</div><div style="float:right;"><div style="float:right;">' + addStarRating(group.rating) + '</div><br><div style="float:right;"><span class="mup-tlabel">Group Rating</span></div></div></div>\
+                    <div class="mup-getwdgt">' + addLink('ADD THIS TO YOUR SITE', 'http://www.meetup.com/meetup_api/foundry/#' + $parameters._name.toLowerCase().replace(/ /g, "-")) + '</div>\
                 </div>\
             </div>'
             );
 
-      $.getJSON($queries.events(), function(data) {
-        if (data.status && data.status.match(/^200/) == null) {
-          alert(data.status + ": " + data.details);
-        } else {
-            if (data.results.length == 0) {
-                 $('.next-event', ctx).append('<span class="mup-tlabel">'+addLink('Suggest new ideas for Meetups!',group.link)+'</span>');
-            } else {
-                var event = data.results[0];
-                console.log(event);
-                var venue = event.venue;
-                console.log(venue);
-                var city;
-                if (!venue || !venue.city) {
-                    city = group.city;
+            $.getJSON($queries.events(), function(data) {
+                if (data.status && data.status.match(/^200/) == null) {
+                    alert(data.status + ": " + data.details);
                 } else {
-                    city = venue.city;
-                }
-                var state_country;
-                if (!venue || !venue.state) {
-                    if (group.state == "") {
-                        state_country = group.country.toUpperCase();
+                    if (data.results.length == 0) {
+                        $('.next-event', ctx).append('<span class="mup-tlabel">' + addLink('Suggest new ideas for Meetups!', group.link) + '</span>');
                     } else {
-                        state_country = group.state;
+                        var event = data.results[0];
+                        console.log(event);
+                        var venue = event.venue;
+                        console.log(venue);
+                        var city;
+                        if (!venue || !venue.city) {
+                            city = group.city;
+                        } else {
+                            city = venue.city;
+                        }
+                        var state_country;
+                        if (!venue || !venue.state) {
+                            if (group.state == "") {
+                                state_country = group.country.toUpperCase();
+                            } else {
+                                state_country = group.state;
+                            }
+                        } else {
+                            state_country = venue.state;
+                        }
+                        var venue_addr;
+                        if (venue) {
+                            if (venue.name !== undefined) {
+                                venue_addr = venue.name + " - ";
+                            } else if (venue.address_1 !== undefined) {
+                                venue_addr = venue.address_1 + " - ";
+                            } else {
+                                venue_addr = "";
+                            }
+                        } else {
+                            venue_addr = "";
+                        }
+                        var location = venue_addr + city + ", " + state_country;
+                        $('.next-event', ctx).append('<h4><div class="mup-tlabel">' + getFormattedDate(event.time) + '   |   ' + getFormattedTime(event.time) + "</div>" + addLink(event.name, event.event_url) + '<div class="mup-tlabel">' + location + "</div></h4>");
                     }
-                } else {
-                    state_country = venue.state;
                 }
-                var venue_addr;
-                if (venue) {
-                    if (venue.name !== undefined) {
-                        venue_addr = venue.name  + " - ";
-                    } else if (venue.address_1 !== undefined) {
-                        venue_addr = venue.address_1 + " - ";
-                    } else {
-                        venue_addr = "";
-                    }
-                } else {
-                   venue_addr = "";
-                }
-                var location = venue_addr + city + ", " + state_country;
-                $('.next-event', ctx).append('<h4><div class="mup-tlabel">'+getFormattedDate(event.time) + '   |   ' + getFormattedTime(event.time) + "</div>" + addLink(event.name, event.event_url)+'<div class="mup-tlabel">' + location + "</div></h4>");
-            }
+            });
         }
-        });
-    }
-  });
+    });
 });
